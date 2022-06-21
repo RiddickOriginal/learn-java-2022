08ceaddb0231b21e37213c353fdb0dbfc5556254
Vyacheslav Vereshchak <vvd@irbis.plus>
2022-06-21 20:29:13 +0400

    Урок 2 - Модификаторы доступа - Методы

---
+++ src/main/java/org/irbis/BaseWithArray.java
@@ -0,0 +1,59 @@
+package org.irbis;
+
+public class BaseWithArray {
+    public static void main(String[] args) {
+        float pi = 3.14F;
+        int studentsCount = 6;
+        long bigNumber = 100000000000L;
+        boolean isEarthFlat = false;
+        char a = 'a';
+        String name = "Славчик";
+
+        int plusOne = studentsCount + 1;
+        int minusTen = studentsCount - 10;
+        int divedTwo = studentsCount / 2;
+        int multiplyTree = studentsCount * 3;
+        boolean isEven = studentsCount % 2 == 0;
+
+        if (!isEarthFlat) {
+            System.out.println("Многа студентов");
+        } else {
+            System.out.println("Никто не пришёл...");
+        }
+
+        switch (studentsCount % 2) {
+            case 0:
+                System.out.println();
+                break;
+            case 1:
+                System.out.println();
+                break;
+            default:
+                System.out.println();
+        }
+
+        int count = 0;
+        while (count != 10) {
+            System.out.println(count);
+            count = count + 1;
+        }
+        System.out.println("Loop end");
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        byte[][] twoDimensionArr = {
+                {1, 1, 1},
+                {0, 1, 0},
+                {0, 1, 0}
+        };
+
+        for (int i = 0; i < twoDimensionArr.length; i++) {
+            for (int j = 0; j < twoDimensionArr[0].length; j++) {
+                System.out.print(" "+twoDimensionArr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/main/java/org/irbis/MethodsAndModifications.java
@@ -0,0 +1,45 @@
+package org.irbis;
+
+public class MethodsAndModifications {
+
+    //            (default)
+    // public
+    // protected
+    // private
+    public static String printNewLine() {
+        String str = "xxxxx";
+        System.out.println("Делаю много логики");
+        System.out.println(str);
+        return str;
+    }
+
+    public static void main(String[] args) {
+        String result = printNewLine();
+        System.out.println(result);
+        int sum = sum(10, 23);
+        int sum1 = sum(16, 58);
+        System.out.println(sum);
+        System.out.println(sum1);
+        System.out.println(sum(1111, 112234));
+
+        minus(100);
+    }
+
+    public static int sum(int x, int y) {
+        return x + y;
+    }
+    public static long sum(long x, long y) {
+        return x + y;
+    }
+
+
+    public static void minus(int x) {
+        if (x != 0) { // ! =
+            System.out.println(x);
+            minus(--x);
+        } else {
+            System.out.println("Я всё!");
+        }
+    }
+
+}
